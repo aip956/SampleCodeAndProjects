f67fcfe1d1ff9a4c3b28515432a5bcc88e5222e0
Anthea Ip <antheaip964@gmail.com>
2022-04-05 21:59:35 -0400

    Saving Express exercise

--- Express/Frank/app.js
+++ Express/Frank/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 const songs = require('./models/songs.js');
 const myMax = songs.length - 1;
 const myMin = 0;
@@ -15,36 +15,42 @@ app.get('/public', function(req, res) {
 })
 
 
-// https://www.geeksforgeeks.org/how-to-check-user-authentication-in-get-method-using-node-js/?ref=gcse
-app.get("/protected",(req,res,next)=>{
-    // Checking the header of the authorization
-   var authheader=req.headers.authorization;
-   console.log(authheader)
-   if(!authheader){     
-       var err=new Error("You are not authenticated")
-       // Set the header for the response
-       res.setHeader("WWW-Authenticate",'Basic')
-       err.status=401
-       return next(err)
-   }
-   console.log(authheader)
- 
-   // Decrypt the user name and the password
-   var auth = new Buffer.from(authheader.split(' ')[1],
-   'base64').toString().split(':');
-   var user = auth[0];
-   var pass = auth[1];
- 
-   // Checking the details
-   if (user == 'admin' && pass == 'admin') {
-     res.send("Welcome, authenticated client")
-   } else {
-       var err = new Error('401 Not authorized');
-       res.setHeader('WWW-Authenticate', 'Basic');
-       err.status = 401;
-       return next(err);
-   }
+app.get('/protected', basicAuth(
+    { users: { 'admin': 'admin' }
+}), function(req, res) {
+    res.send("Welcome, authenticated client");
 })
+
+// https://www.geeksforgeeks.org/how-to-check-user-authentication-in-get-method-using-node-js/?ref=gcse
+// app.get("/protected",(req,res,next)=>{
+//     // Checking the header of the authorization
+//    var authheader=req.headers.authorization;
+//    console.log(authheader)
+//    if(!authheader){     
+//        var err=new Error("You are not authenticated")
+//        // Set the header for the response
+//        res.setHeader("WWW-Authenticate",'Basic')
+//        err.status=401
+//        return next(err)
+//    }
+//    console.log(authheader)
+ 
+//    // Decrypt the user name and the password
+//    var auth = new Buffer.from(authheader.split(' ')[1],
+//    'base64').toString().split(':');
+//    var user = auth[0];
+//    var pass = auth[1];
+ 
+//    // Checking the details
+//    if (user == 'admin' && pass == 'admin') {
+//      res.send("Welcome, authenticated client")
+//    } else {
+//        var err = new Error('401 Not authorized');
+//        res.setHeader('WWW-Authenticate', 'Basic');
+//        err.status = 401;
+//        return next(err);
+//    }
+// })
 
 
 app.get('/', function(req, res) {
@@ -65,17 +71,15 @@ app.get('/wives', function(req, res) {
 })
 
 app.get('/picture', function(req, res) {
-    res.send(`
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Frank_Sinatra_%281957_studio_portrait_close-up%29.jpg/455px-Frank_Sinatra_%281957_studio_portrait_close-up%29.jpg" >
-    `);
+    res.redirect('https://upload.wikimedia.org/wikipedia/commons/a/af/Frank_Sinatra_%2757.jpg');
 })
 
 
 
-app.post('/', function (req, res) {
-    console.log(req.body);
-    res.end();
-});
+// app.post('/', function (req, res) {
+//     console.log(req.body);
+//     res.end();
+// });
 
 function getSong(myMin, myMax) {
     var i = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
