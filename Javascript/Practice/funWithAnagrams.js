d1eab57d4d569da7a0af5704007450931e61386d
Anthea Ip <antheaip964@gmail.com>
2022-05-05 16:20:54 -0400

    Updated funWithAn

--- Javascript/Practice/funWithAnagrams.js
+++ Javascript/Practice/funWithAnagrams.js
@@ -1,35 +1,12 @@
-function funWithAnagrams(str){
-    // console.log(str);
-      var TempArray = [];
-      var outputArray = [];
-      var strSorted = str.sort();
-    // console.log(strSorted);
-    for (i = 0; i < str.length; i++) {
-      var strByEl =str[i].split(',');
-      // console.log(strByEl.sort());
-      // console.log(strByEl);
-      var origElem = str[i];
-      // console.log(origElem);
-      var elementbrk = (str[i].split('').sort());
-      // console.log(elementbrk);
-      var element = (elementbrk.join(''));
-      // console.log(element);
-        //     TempArray.push(element);
-        // console.log(TempArray);
-      if (!TempArray.includes(element)) {
-        TempArray.push(element);
-        outputArray.push(origElem);
-        // console.log(TempArray);
-        // console.log(outputArray);
-      };
-    }
-        return(outputArray);
-  }
-  
+// Fun with Anagrams
+// Given an array of strings, remove each string that is an anagram of an earlier string, then return the remaining array in sorted order.
 
-  console.log(funWithAnagrams(['12345', '1234', 'hello', 'othello', 'hello']));
-  
-  // Split array by elements, sort the elements in array
+// Example:
+// Input: str = ['code', 'doce', 'ecod', 'framer', 'frame']
+// Output: ['code','frame','framer']
+
+// Logic:
+// Split array by elements, sort the elements in array
   // Split each element and sort it
   // For each sorted element, is it in the TempArray?
   // If not, push it to the TempArray, and push the original element to the outputArray
@@ -45,4 +22,27 @@ function funWithAnagrams(str){
   // If the TempArray does not include the element (sorted word), 
   // => push it to the TempArray
   // => and then push the original word, origElem, to the output array
+
+
+function funWithAnagrams(str){
+      var TempArray = [];
+      var outputArray = [];
+      var strSorted = str.sort();
+    for (i = 0; i < str.length; i++) {
+      var strByEl =str[i].split(',');
+      var origElem = str[i];
+      var elementbrk = (str[i].split('').sort());
+      var element = (elementbrk.join(''));
+          if (!TempArray.includes(element)) {
+        TempArray.push(element);
+        outputArray.push(origElem);
+       };
+    }
+        return(outputArray);
+  }
+  
+
+  console.log(funWithAnagrams(['12345', '1234', 'hello', 'othello', 'hello']));
+  
+  
   
