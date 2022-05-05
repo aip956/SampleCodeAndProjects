ca0b78f88aac52f7703d7c3e2b76f8523df13d12
Anthea Ip <antheaip964@gmail.com>
2022-05-05 16:24:37 -0400

    Added ProdExcSelf.js

---
+++ Javascript/Practice/ProdExcSelf.js
@@ -0,0 +1,26 @@
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+ 
+// Example 1:
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+// Example 2:
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
+
+
+function productExceptSelf(nums){
+    var output = [];
+      for (i = 0; i < nums.length; i++) {
+        var elem = 1;
+        for (j = 0; j < nums.length; j++) {
+          if (i != j) {
+            elem = elem*nums[j]
+          }
+        }
+      output.push(elem);
+      }
+    return output;
+    }
