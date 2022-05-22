97154047517658d5d13f1c62e8d3a732a662deaa
Anthea Ip <antheaip964@gmail.com>
2022-05-22 15:00:55 -0400

    in process of threeSum

---
+++ Javascript/Practice/threeSum.js
@@ -0,0 +1,42 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+var threeSum = function(nums) {
+    var i = 0;
+    var j = 0;
+    var k = 0;
+    var array = [];
+    var holdingArr = [];
+    var arrOfSorts = [];
+    var outputArr = [];
+    for (i = 0; i < nums.length-2; i++) {
+        for (j = i+1; j < nums.length-1 && i != j; j++) {
+            for (k = j+1; k < nums.length && i != j != k; k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    array = [nums[i], nums[j], nums[k]];
+
+                   
+
+                    // sortedArr = array.sort();
+                    // console.log("Array", array);
+                    // console.log("Sorted", sortedArr);
+                    // console.log("ArrOfSorts", arrOfSorts)
+                    // console.log(!(arrOfSorts.includes(sortedArr)))
+                    // console.log((arrOfSorts.includes(sortedArr)))
+                   console.log(array.every(element => {holdingArr.includes(element); }));
+                    ;
+                    // if (!(arrOfSorts.includes(sortedArr))) {
+                    //     arrOfSorts.push(sortedArr);
+                    //     console.log("23ArrOfSorts", arrOfSorts)
+                    //     console.log("24", output);
+                    //     output.push(array);
+                    //     console.log("26", output);
+                    // }
+                    
+                }; // closes if
+            }; // closes for k
+        }; // closes for j
+    }; // closes for i
+    return outputArr;
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
