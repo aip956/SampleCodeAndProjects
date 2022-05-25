cec325dbb6d40d381d649f5bf326663749d40086
Anthea Ip <antheaip964@gmail.com>
2022-05-25 16:28:29 -0400

    Edited threeSum

--- Javascript/Practice/threeSum.js
+++ Javascript/Practice/threeSum.js
@@ -5,7 +5,6 @@ var threeSum = function(nums) {
     var j = 0;
     var k = 0;
     var array = [];
-    var holdingArr = [];
     var arrOfSorts = [];
     var outputArr = [];
     for (i = 0; i < nums.length-2; i++) {
@@ -13,25 +12,21 @@ var threeSum = function(nums) {
             for (k = j+1; k < nums.length && i != j != k; k++) {
                 if (nums[i] + nums[j] + nums[k] == 0) {
                     array = [nums[i], nums[j], nums[k]];
-
-                   
-
-                    // sortedArr = array.sort();
-                    // console.log("Array", array);
-                    // console.log("Sorted", sortedArr);
+                    console.log("ijk",i,j,k)
+                    sortedArr = array.sort();
+                    console.log("Array", array);
+                    console.log("Sorted", sortedArr);
                     // console.log("ArrOfSorts", arrOfSorts)
                     // console.log(!(arrOfSorts.includes(sortedArr)))
                     // console.log((arrOfSorts.includes(sortedArr)))
-                   console.log(array.every(element => {holdingArr.includes(element); }));
-                    ;
-                    // if (!(arrOfSorts.includes(sortedArr))) {
-                    //     arrOfSorts.push(sortedArr);
-                    //     console.log("23ArrOfSorts", arrOfSorts)
-                    //     console.log("24", output);
-                    //     output.push(array);
-                    //     console.log("26", output);
-                    // }
-                    
+                   console.log("25 arrSort incl sorted", array.every(element => {arrOfSorts.includes(element); }));
+                   console.log("26 arrSort not incl sorted", !array.every(element => {arrOfSorts.includes(element); }));
+                    if (!array.every(element => {arrOfSorts.includes(element)})) {
+                        arrOfSorts.push(sortedArr);
+                        console.log("26 arrOfSorts", arrOfSorts);
+                        outputArr.push(array);
+                        console.log("28 outp Arr", outputArr);
+                        }
                 }; // closes if
             }; // closes for k
         }; // closes for j
@@ -40,3 +35,4 @@ var threeSum = function(nums) {
 };
 
 console.log(threeSum([-1,0,1,2,-1,-4]));
+
