d91d0dec1835994a2a2a8c86f43407e9ae119d0e
Anthea Ip <antheaip964@gmail.com>
2022-05-31 15:41:35 -0400

    Adding

---
+++ Javascript/Problems/inProcess/minCostTickets.js
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/minimum-cost-for-tickets/
+// You have planned some train traveling one year in advance. The days of the year in which you will travel are given as an integer array days. Each day is an integer from 1 to 365.
+
+// Train tickets are sold in three different ways:
+
+// a 1-day pass is sold for costs[0] dollars,
+// a 7-day pass is sold for costs[1] dollars, and
+// a 30-day pass is sold for costs[2] dollars.
+// The passes allow that many days of consecutive travel.
+
+// For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
+// Return the minimum number of dollars you need to travel every day in the given list of days.
+
+ 
+
+// Example 1:
+
+// Input: days = [1,4,6,7,8,20], costs = [2,7,15]
+// Output: 11
+// Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
+// On day 1, you bought a 1-day pass for costs[0] = $2, which covered day 1.
+// On day 3, you bought a 7-day pass for costs[1] = $7, which covered days 3, 4, ..., 9.
+// On day 20, you bought a 1-day pass for costs[0] = $2, which covered day 20.
+// In total, you spent $11 and covered all the days of your travel.
+// Example 2:
+
+// Input: days = [1,2,3,4,5,6,7,8,9,10,30,31], costs = [2,7,15]
+// Output: 17
+// Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
+// On day 1, you bought a 30-day pass for costs[2] = $15 which covered days 1, 2, ..., 30.
+// On day 31, you bought a 1-day pass for costs[0] = $2 which covered day 31.
+// In total, you spent $17 and covered all the days of your travel.
+
+
+
+
+// If 1 day travel, get 1-day pass
+// If 2 consec days, lessor of 2 x 1 or 7-day
+// If 3 consec days, lessor of 3 x 1 or 7-day
+// If days[i+1] - days[i] 
+// days = [1,4,6,7,8,20]
+// days[1] - days[0] = 4 - 1 - 1 = 2
+// days[2] - days[1] = 6 - 4 -1 = 1
+
+// for (ind = 0; ind < days.length; ind++)
+// for 1 day: days[0] = 1, end on day 1 (1 + 1 - 1) day+ticket-1
+// => cost of all 1-day ticks = costs[0]*days.length
+
+// for 7 day: days[0] = 1, end on day 7 (1 + 7 - 1)
+// is days[i] >= endOnDayOf7
+// => for days[4] = 8, end on day 10 (4+7-1)
+// => => for 1 day: days[4] = 8, end on day 8
+// => => for 7 day: days[4] = 8, end on day 14 (8 + 7 - 1)
+// => => for 30 day: days[4] = 8, end on day 37 (8 + 30 - 1)
+
+// for 30 day: days[0] = 1, end on day 30 (1 + 30 - 1)
+// days[1] = 4; less than endOnDay for 1 day ticket
+// => Would need to buy another
+// => for 1 day: days[0] = 1, end on day 1 (1 + 1 - 1)
+// stack data structure
