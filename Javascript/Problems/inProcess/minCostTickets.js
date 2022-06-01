6005356516f08f35809869baf8b21aaf040966de
Anthea Ip <antheaip964@gmail.com>
2022-05-31 21:30:52 -0400

    Working on minCost

--- Javascript/Problems/inProcess/minCostTickets.js
+++ Javascript/Problems/inProcess/minCostTickets.js
@@ -42,16 +42,44 @@
 // days[1] - days[0] = 4 - 1 - 1 = 2
 // days[2] - days[1] = 6 - 4 -1 = 1
 
+// costs = [2,7,15]
+// costs[0]*4 > costs[1]; so if there are 4 consec days, cheaper to buy 7-day
+// factor_0 = Math.ceil(costs[1]/costs[0]) 
+// costs[1]*3 > costs[2]; if there are > 7*3 consec days, cheaper to buy 15-day
+// factor_1 = Math.ceil(costs[2]/costs[1]) 
+
+
+
+var mincostTickets = function(days, costs) {
+    factor7 = Math.ceil(costs[1]/costs[0]);
+    factor30 = Math.ceil(costs[2]/costs[1]);
+    console.log(factor7, factor30);
+
+
+};
+
+days = [1,2,3,4,5,6,7,8,9,10,30,31];
+costs = [2,7,15];
+console.log(mincostTickets(days, costs));
+
+// elemPlus7: day[i] + 7; days that 7-day is valid
+// elemPlus30: day[i] + 30; days that 30-day is valid
+// if there are > factor_0 elements in 7-day period, buy 7-day
+// if there are > factor_1 elements in 30-day period, buy 30-day
+
 // for (ind = 0; ind < days.length; ind++)
 // for 1 day: days[0] = 1, end on day 1 (1 + 1 - 1) day+ticket-1
 // => cost of all 1-day ticks = costs[0]*days.length
 
 // for 7 day: days[0] = 1, end on day 7 (1 + 7 - 1)
-// is days[i] >= endOnDayOf7
+// is days[i] >= endOnDayOf7 
 // => for days[4] = 8, end on day 10 (4+7-1)
 // => => for 1 day: days[4] = 8, end on day 8
+// => => => cost = cost + costs[0]
 // => => for 7 day: days[4] = 8, end on day 14 (8 + 7 - 1)
+// => => => cost = cost + costs[1]
 // => => for 30 day: days[4] = 8, end on day 37 (8 + 30 - 1)
+// => => => cost = cost + costs[2]
 
 // for 30 day: days[0] = 1, end on day 30 (1 + 30 - 1)
 // days[1] = 4; less than endOnDay for 1 day ticket
