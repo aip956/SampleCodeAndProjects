b5d572f533a1b2a4f0d6c134f4778ab6622eea11
Anthea Ip <antheaip964@gmail.com>
2022-09-17 10:20:22 -0700

    Game state draft

--- C/MMv2/draft.c
+++ C/MMv2/draft.c
@@ -45,9 +45,9 @@ int my_strlen(char* param_1) {
     game_state_ptr->secret[i] = '\0';
     printf("48: %c\n", game_state_ptr->secret[2]);
     printf("49: %s\n", game_state_ptr->secret);
-    // printf("50: %c\n", *game_state_ptr);
 
-} // Closes function
+
+}; // Closes function
 
 int is_code_valid(char* code, int code_len) {
     int is_code_valid = 1;
@@ -77,76 +77,41 @@ void make_secret (char** av, int ac, t_game_state* game_state_ptr) {
         for (i = 0; i < 2; i++) {
             if (flag_c[i] == flag[i]) {
                 count ++;
-                printf("count: %d\n", count);
-                if (count == 2) { // found -c
+                if (count == 2) { 
                     isFlagC = 1; 
-                    printf("87 \n");
-                    printf("93 av ind+1: %s\n", av[av_index + 1]);
-                        if (is_code_valid(av[av_index + 1], 4) == 1) {
-                            char* secret = av[av_index + 1];
-                            printf("88 sec: %c\n", *secret);
-                            printf("89 sec: %s\n", secret);  
-                            printf("100 \n");
-                        }
-                }
-//             printf("102 \n");
-            }     // closes search on av element
-//         printf("104 \n");
-        }; // closes for av
-//     printf("106 \n");
-    }
-        printf("isflagc: %d\n", isFlagC);
+                    if (is_code_valid(av[av_index + 1], 4) == 1) {
+                        for (int secr_ind = 0; secr_ind < 4; secr_ind++) {
+                            game_state_ptr->secret[secr_ind] = av[av_index + 1][secr_ind];
+                        };
+                    };
+                }; 
+            };     
+        }; 
+    };
         if (isFlagC == 0) {
             printf("101isflagc: %d\n", isFlagC);
-
-            // char rand_secret_arr[4]="";
-            // char ranNum;
-            // int i_of_ran_secr;
-            // srand(time(NULL));
-            // for (i_of_ran_secr = 0; i_of_ran_secr < 4; i_of_ran_secr++) {
-            //     ranNum =  (48 + (rand() % 8));
-            //     rand_secret_arr[i_of_ran_secr] = ranNum;
-            //     printf("%c\n", ranNum);
-            //     printf("118 rand_sec[i]: %d, %c\n", i_of_ran_secr, rand_secret_arr[i_of_ran_secr]);
-            // } // Closes for int i
-            // rand_secret_arr[i_of_ran_secr] = '\0';
             rand_secret(game_state_ptr);
-            // printf("109rand_secr: %s\n", secret); 
-
         };
-    printf("110 \n");
-    // printf("121*sec: %c\n", *secret);
-    // printf("122sec: %s\n", secret);
-
-}; // Closes make_secret
+}; 
 
 int make_tries (char** av, int ac){
     int i, av_index;
-    printf("make tries");
     int tries = 10;
     char *flag_t = "-t";
     int count = 0;
-    printf("129 \n");
     for (av_index = 1; av_index < ac; av_index++) {
-        printf("130, av_ind: %d\n", av_index);
         char* flag = av[av_index];
-        printf("132 flag: %s\n", flag);
         for (i = 0; i < 2; i++) {
             if (flag_t[i] == flag[i]) {
                 count ++;
-                printf("131count: %d\n", count);
                 if (count == 2) { // found -t
-                    printf("138 \n");
                     tries = atoi(av[av_index + 1]);
-                    printf("140 av_ind+1: %d\n", av_index + 1);
-                    printf("141 av ind+1: %s\n", av[av_index + 1]);
-                    printf("142 tries: %d\n",tries);
-                } // Closes isFlagT
-            }
-        }  
-    }
+                }; 
+            };
+        }; 
+    };
     return tries;
-}
+};
 
 
 t_game_state prep_init_state(int ac, char** av) {
@@ -156,9 +121,6 @@ t_game_state prep_init_state(int ac, char** av) {
     };
     t_game_state *game_state_ptr = &game_state;
     make_secret(av, ac, game_state_ptr);
-    printf("162 tries: %d\n", game_state.tries);
-    printf("163 secr: %s\n", game_state.secret);
-    printf("163 secr: %c\n", game_state.secret[2]);
     return game_state;
 };
 
@@ -167,15 +129,10 @@ t_game_state prep_init_state(int ac, char** av) {
 
 
 int main(int ac, char** av) {
-    // printf("142_av: %s\n", av[1]);
-    // printf("143_av: %s\n", av[2]);
     t_game_state game_state;
     game_state = prep_init_state(ac, av);
     printf("156 tries: %d\n", game_state.tries);
     printf("157 secr: %s\n", game_state.secret);
-
-    // play_game(game_state);
-
 return 0;
 }
 
@@ -183,14 +140,3 @@ return 0;
 
 // ./draft -c "0123" -t "4"
 // gcc -o draft draft.c
-
-
-
-
-      // for (i = 0; i < 4; i++) {
-                        //     printf("88sec_in_av_ind : %d\n", i);
-                        //     printf("89secret[i]: %s\n", av[av_index + 1]);
-                        //     printf("90secret[i][secinav]: %c\n", av[av_index + 1][i]);
-                        //     *secret[i] = av[av_index + 1][i];
-                        //     printf("93secret[i]: %c\n", *secret[i]);
-                        //     }
