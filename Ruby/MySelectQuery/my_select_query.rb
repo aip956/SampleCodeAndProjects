db1e8b0930485c8b8bfe2e46e9e8511d2f82c5b3
Anthea Ip <70281423+aip956@users.noreply.github.com>
2023-02-28 20:30:18 -0500

    Delete Ruby directory

--- Ruby/MySelectQuery/my_select_query.rb
+++
@@ -1,7 +0,0 @@
-dev my_select_query(string)
-
-
-end
-
-puts (my_select_query("name,year_start,year_end,position,height,weight,birth_date,college\nAlaa Abdelnaby,1991,1995,F-C,6-10,240,'June 24, 1968',Duke University\nZaid Abdul-Aziz,1969,1978,C-F,6-9,235,'April 7, 1946',Iowa State University\nKareem Abdul-Jabbar,1970,1989,C,7-2,225,'April 16, 1947','University of California, Los Angeles
-Mahmoud Abdul-Rauf,1991,2001,G,6-1,162,'March 9, 1969',Louisiana State University\n"))
