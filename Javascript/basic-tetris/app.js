cc8d724dff01f8c20d8403df92866d12c407d0f9
Anthea Ip <antheaip964@gmail.com>
2023-12-11 20:06:41 -0500

    Save

--- Javascript/basic-tetris/app.js
+++ Javascript/basic-tetris/app.js
@@ -5,8 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button')
     const width = 10
 
-    
+
     console.log("squares:", squares);
+// The Tetrominoes
+const lTet = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+]
+
+const zTet = [
+    [0, width, width+1, width*2+1],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+]
+
+
 
 
 
