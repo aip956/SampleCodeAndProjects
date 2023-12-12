a888a6ac1673332a58b145ed98984639102df255
Anthea Ip <antheaip964@gmail.com>
2023-12-11 19:28:07 -0500

    Save

--- Javascript/basic-tetris/app.js
+++ Javascript/basic-tetris/app.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+
+    
+    console.log("squares:", squares);
 
 
 
-
-
-})
+}) 
